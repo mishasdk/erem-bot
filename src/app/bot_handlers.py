3a7ba4493ec1d5b7afef866f8706a6d2821e6366
mishasdk <mi.serdyukov@yandex.ru>
2021-07-17 19:31:35 +0200

    Added poll

--- src/app/bot_handlers.py
+++ src/app/bot_handlers.py
@@ -1,33 +1,79 @@
 from naruto_quotes_provider import NarutoQuotesProvider
 from random import randint
 from telegram.ext import CommandHandler, MessageHandler, Filters
-from utils import Utils
-
+from users_storage import UsersStorage 
 
 class BotHandlers:
     _naruto_quotes_provider = NarutoQuotesProvider()
+    _users_storage = UsersStorage()
 
     def __init__(self):
         self._handlers = (
             CommandHandler('start', self._start),
             CommandHandler('help', self._help),
             CommandHandler('roll', self._roll),
-            CommandHandler('naruto', self._naruto)
+            CommandHandler('naruto', self._naruto),
+            CommandHandler('who_is_dumb', self._poll_dumb)
         )
 
     def handlers(self):
         return self._handlers
 
     def _start(self, update, context):
+        self._save_username(update)
+
         update.message.reply_text('Привет всем, ребята!')
 
     def _help(self, update, context):
+        self._save_username(update)
+
         update.message.reply_text('Всегда готов помочь!))')
 
     def _roll(self, update, context):
+        self._save_username(update)
+
         update.message.reply_text(str(randint(1, 100)))
 
     def _naruto(self, update, context):
+        self._save_username(update)
+
         quote = self._naruto_quotes_provider.provide()
         message = f"{quote['text']}\n\n©{quote['author']}"
         update.message.reply_text(message)
+
+    def _poll_dumb(self, update, context):
+        self._save_username(update)
+
+        chat_id = update.effective_chat.id        
+        username = BotHandlers._username_by_user(update.effective_user)
+
+        usernames = self._users_storage.get_usernames(chat_id)
+        if len(usernames) < 2:
+            context.bot.send_message(chat_id, f'Не могу начать голосование, я еще не достаточно вас знаю!')
+            return
+
+        context.bot.send_message(chat_id, f'{username} начинает голосование!')
+        context.bot.send_poll(
+            chat_id,
+            'Кто пидор?',
+            usernames,
+            is_anonymous=True,
+            allows_multiple_answers=True,
+            open_period=300
+        )
+
+    def _save_username(self, update):
+        chat_id = update.effective_chat.id        
+        self._users_storage.try_save_user(update.effective_user, chat_id)
+
+    @staticmethod
+    def _username_by_user(user):
+        username = ''
+        if not user.first_name:
+            username = user.username
+        else:
+            if user.first_name:
+                username = user.first_name 
+            if user.last_name:
+                username += f" {user.last_name}"
+        return username
